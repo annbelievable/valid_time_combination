e3e45794aae26e885e6680f7aba1217263c2439a
kokann <kokann_92@hotmail.com>
2022-08-29 22:54:35 +0800

    Implement a simple permutation function

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+var combinations [][]string
 
 func main() {
-	fmt.Println("hello world")
+	example := []string{"1", "2", "3", "4"}
+	heapPermutation(example, len(example))
+	for _, comb := range combinations {
+		fmt.Println(comb)
+	}
+}
+
+// heap alroithm
+func heapPermutation(s []string, size int) {
+	// if size becomes 1 then append the result to combinations
+	if size == 1 {
+		var res = make([]string, 4)
+		copy(res, s)
+		combinations = append(combinations, res)
+		return
+	}
+
+	for i := 0; i < size; i++ {
+		heapPermutation(s, size-1)
+		// if size is odd, swap first and last element
+		// if size is even, swap ith and last element
+		if size%2 != 0 {
+			s[0], s[size-1] = s[size-1], s[0]
+		} else {
+			s[i], s[size-1] = s[size-1], s[i]
+		}
+
+	}
 }
