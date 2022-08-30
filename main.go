2eab042e963e9ee3501174b76c191a2ebefe80b6
kokann <kokann_92@hotmail.com>
2022-08-30 21:52:26 +0800

    Used a hardcoded way to calculate the number of valid times from a given array of integers

--- main.go
+++ main.go
@@ -4,21 +4,43 @@ import (
 	"fmt"
 )
 
-var combinations [][]string
+var combinations [][]int
 
 func main() {
-	example := []string{"1", "2", "3", "4"}
-	heapPermutation(example, len(example))
-	for _, comb := range combinations {
-		fmt.Println(comb)
-	}
+	example := []int{1, 2, 3, 4}
+	checkTotalValidTimes(example)
 }
 
-// heap alroithm
-func heapPermutation(s []string, size int) {
+func checkTotalValidTimes(s []int) {
+	if len(s) != 4 {
+		panic("4 integers are required")
+	}
+
+	for _, v := range s {
+		if v < 0 || v > 9 {
+			panic("Integers has to be between 0 and 9")
+		}
+	}
+
+	heapPermutation(s, len(s))
+	totalValidTimes := 0
+	for _, comb := range combinations {
+		isValid := isValidTime(comb)
+		fmt.Println(comb, isValid)
+
+		if isValid {
+			totalValidTimes++
+		}
+	}
+
+	fmt.Println("Total valid times: ", totalValidTimes)
+}
+
+// heap algorithm
+func heapPermutation(s []int, size int) {
 	// if size becomes 1 then append the result to combinations
 	if size == 1 {
-		var res = make([]string, 4)
+		var res = make([]int, 4)
 		copy(res, s)
 		combinations = append(combinations, res)
 		return
@@ -35,4 +57,26 @@ func heapPermutation(s []string, size int) {
 		}
 
 	}
+}
+
+func isValidTime(s []int) bool {
+	// check hours
+	if s[0] > 2 {
+		return false
+	}
+
+	if s[0] == 2 && s[1] > 3 {
+		return false
+	}
+
+	// check minutes
+	if s[2] > 5 {
+		return false
+	}
+
+	if s[3] > 9 {
+		return false
+	}
+
+	return true
 }
